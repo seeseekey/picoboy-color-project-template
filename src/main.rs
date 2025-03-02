12d72ea25b9d460d857719cec2cda3ddaaa8bfcc
seeseekey <info@seeseekey.net>
2025-03-02 10:28:57 +0100

    Build Picoboy Color template project.

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,19 @@ use panic_probe as _;
 
 use rp2040_hal::Spi;
 
-use embedded_graphics::mono_font::ascii::FONT_6X10;
-use embedded_graphics::mono_font::MonoTextStyleBuilder;
-use embedded_graphics::pixelcolor::BinaryColor;
-use embedded_graphics::prelude::*;
-use embedded_graphics::text::{Baseline, Text};
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyleBuilder},
+};
 use embedded_hal::digital::{InputPin, OutputPin};
 
-use sh1106::prelude::GraphicsMode;
-use sh1106::Builder;
+use display_interface_spi::SPIInterface;
+use st7789::{Orientation, ST7789};
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use picoboy as bsp;
+use picoboy_color as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
@@ -29,7 +29,6 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
-use embedded_graphics::primitives::{Circle, PrimitiveStyleBuilder};
 use rp2040_hal::fugit::RateExtU32;
 
 #[entry]
@@ -65,8 +64,12 @@ fn main() -> ! {
     );
 
     // Define display width and height
-    const DISPLAY_WIDTH: i32 = 128;
-    const DISPLAY_HEIGHT: i32 = 64;
+    const DISPLAY_WIDTH: i32 = 240;
+    const DISPLAY_HEIGHT: i32 = 280;
+
+    // Switch on backlight
+    let mut backlight = pins.backlight.into_push_pull_output();
+    backlight.set_high().unwrap();
 
     // Configure SPI pins
     let spi_sclk = pins.sck.into_function::<rp2040_hal::gpio::FunctionSpi>(); // SCK
@@ -80,51 +83,32 @@ fn main() -> ! {
     let spi = spi.init(
         &mut pac.RESETS,
         clocks.peripheral_clock.freq(),
-        8_000_000u32.Hz(),         // 8 MHz
-        embedded_hal::spi::MODE_0, // MODE_0 for SH1106
+        125000000u32.Hz(),
+        embedded_hal::spi::MODE_3, // ST7789 requires SPI mode 3
     );
 
     // Configure display pins
     let dc = pins.dc.into_push_pull_output(); // Data/command pin
-    let mut rst = pins.reset.into_push_pull_output(); // Reset pin
+    let rst = pins.reset.into_push_pull_output(); // Reset pin
     let cs = pins.cs.into_push_pull_output(); // Chip select
 
-    // Reset display
-    rst.set_low().unwrap();
-    delay.delay_ms(10);
-    rst.set_high().unwrap();
-    delay.delay_ms(10);
-
     // Create display interface
-    let mut display: GraphicsMode<_> = Builder::new().connect_spi(spi, dc, cs).into();
+    let di = SPIInterface::new(spi, dc, cs);
 
-    display.init().unwrap();
-    display.flush().unwrap();
+    // Create and init display
+    let mut display = ST7789::new(di, rst, DISPLAY_WIDTH as u16, DISPLAY_HEIGHT as u16);
 
-    // Create Hello world.
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
-
-    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
-        .draw(&mut display)
+    display.init(&mut delay).unwrap();
+    display
+        .set_orientation(Orientation::PortraitSwapped)
         .unwrap();
 
-    Text::with_baseline(
-        "From Rust with love.",
-        Point::new(0, 16),
-        text_style,
-        Baseline::Top,
-    )
-    .draw(&mut display)
-    .unwrap();
+    // Clear display
+    display.clear(Rgb565::RED).unwrap();
 
-    display.flush().unwrap();
-    delay.delay_ms(2500);
-
-    // Ball
-    display.clear();
+    // Status led
+    let mut led_pin = pins.led_red.into_push_pull_output();
+    led_pin.set_high().unwrap();
 
     // Configuring joystick buttons
     let mut joystick_up = pins.joystick_up.into_pull_up_input();
@@ -135,16 +119,13 @@ fn main() -> ! {
     let mut x: i32 = DISPLAY_WIDTH / 2;
     let mut y: i32 = DISPLAY_HEIGHT / 2;
 
-    // Clear display
-    display.clear();
+    let mut old_x = 0;
+    let mut old_y = 0;
 
-    // Status led
-    let mut led_pin = pins.led_red.into_push_pull_output();
-    led_pin.set_high().unwrap();
+    // Clear display
+    display.clear(Rgb565::BLACK).unwrap();
 
     loop {
-        display.clear();
-
         // Check entries and adjust position
         if joystick_down.is_low().unwrap() {
             y = y.saturating_add(2);
@@ -162,16 +143,27 @@ fn main() -> ! {
             x = x.saturating_sub(2);
         }
 
-        // Draw a new circle
-        let new_circle = Circle::new(Point::new(x, y), 25).into_styled(
-            PrimitiveStyleBuilder::new()
-                .fill_color(BinaryColor::On)
-                .build(),
-        );
+        if x != old_x || y != old_y {
+            // Only paint over the old circle with black instead of erasing the entire screen
+            let old_circle = Circle::new(Point::new(old_x, old_y), 25).into_styled(
+                PrimitiveStyleBuilder::new()
+                    .fill_color(Rgb565::BLACK)
+                    .build(),
+            );
+            old_circle.draw(&mut display).unwrap();
 
-        new_circle.draw(&mut display).unwrap();
+            // Draw a new circle
+            let new_circle = Circle::new(Point::new(x, y), 25).into_styled(
+                PrimitiveStyleBuilder::new()
+                    .fill_color(Rgb565::MAGENTA)
+                    .build(),
+            );
+            new_circle.draw(&mut display).unwrap();
 
-        display.flush().unwrap();
+            // Update positions
+            old_x = x;
+            old_y = y;
+        }
 
         delay.delay_ms(50);
     }
